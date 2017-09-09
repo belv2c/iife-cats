e0134adc1eb97da08998ee2595ac0719aace4d4d
Brooke Lewis <belv2c@gmail.com>
2017-09-09 14:04:37 -0500

    added more event listeners

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,4 +6,26 @@ document.getElementById("fancyPetStore").addEventListener("click", (event) => {
 	let catIndex = event.target.id.split("-")[1];
 	CatStore.adoptCat(catIndex);
 	}
-})
+});
+
+document.getElementById("callan").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(2);
+});
+
+document.getElementById("homeless").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(0);
+});
+
+document.getElementById("all").addEventListener("click", (event) =>{
+	let cats = CatStore.getCats();
+	let owners = CatStore.getOwners();
+	CatStore.combineArrays(cats, owners);
+});
+
+document.getElementById("lauren").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(3);
+});
+
+document.getElementById("zoe").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(1);
+});
